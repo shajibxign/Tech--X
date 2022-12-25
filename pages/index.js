b14ad12ed2bec0239d887e7c894bd75ca5653085
shajib1927 <shajibshahrear@gmail.com>
2022-12-25 23:36:53 +0300

    7th

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>index</div>
+    <>
+      HeroBanner
+
+      <div className='products-heading'>
+        <h2>BEST SELLING PRODUCTS</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div className='products-container'>
+        {['Product 1', 'Product 2'].map(
+          (product) => product)}
+      </div>
+      
+      Footer
+    </>
+    
   )
 }
 
