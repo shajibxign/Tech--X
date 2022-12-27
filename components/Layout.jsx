8be79f7fe59dff0fba426709ac74f8fa3d46b822
shajib1927 <shajibshahrear@gmail.com>
2022-12-27 16:11:02 +0300

    12th

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-export default Layout;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>TECH--X SHOP</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
