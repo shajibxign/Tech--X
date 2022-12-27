2bd0ded42711376a820b3879462f7621d135888a
shajib1927 <shajibshahrear@gmail.com>
2022-12-27 20:25:52 +0300

    13th

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { AiFillInstagram, AiFillFacebook, AiOutlineTwitter} from 'react-icons/ai';
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  return (
+    <div className="footer-container">
+      <p>2023 TECHX -- All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter/>
+        <AiFillFacebook />
+      </p>
+    </div>
+  )
+}
 
-export default Footer;
+export default Footer
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
+
+// import { Cart } from './';
+// import { useStateContext} from '../context/StateContext';
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+//   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
-export default Navbar;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">TechX Mobiles</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        {/* <span className="cart-item-qty">{totalQuantities}</span> */}
+        <span className="cart-item-qty">1</span>
+      </button>
+
+      {/* {showCart && <Cart />} */}
+    </div>
+  )
+}
+
+export default Navbar
