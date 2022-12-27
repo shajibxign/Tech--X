5cf9f99afce20594125c8de680760bfbb4065595
shajib1927 <shajibshahrear@gmail.com>
2022-12-27 23:20:22 +0300

    16th

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,11 +11,11 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: 'shr_1MJjC7KSh6YHKovYzwliOGdO' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/khb5cp2m/production/').replace('-webp', '.webp');
 
           return {
             price_data: { 
